d0098e02fd1e53a0fa8001cd3264e1ff2eb30488
Karin Marjieh <karinmarjieh@hotmail.com>
2025-02-27 23:34:02 +0200

    changed build settings on vite.config.js

--- oriana/vite.config.js
+++ oriana/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure this is set to 'dist' if you want Vite to output to that directory
+  },
+});
